e7ffe1641531baada827603daedfc77c498ad2c8
ajay kundu <ajaykunduktl2@gmail.com>
2017-08-08 23:25:05 +0530

    more better approach without map

--- 199. right side of binary tree.cpp
+++ 199. right side of binary tree.cpp
@@ -35,3 +35,40 @@ public:
         return v;
     }
 };
+
+
+More Better approach. without extra map space.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void rightSideViewUtil(TreeNode* root,int level,int &maxlevel,vector<int> &v)
+    {
+        if(root==NULL)
+            return ;
+        
+        if(level>maxlevel)
+        {
+            v.push_back(root->val);
+            maxlevel=level;
+        }
+        rightSideViewUtil(root->right,level+1,maxlevel,v);
+        rightSideViewUtil(root->left,level+1,maxlevel,v);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> v;
+        int maxlevel=0;
+        rightSideViewUtil(root,1,maxlevel,v);
+        return v;
+    }
+};
